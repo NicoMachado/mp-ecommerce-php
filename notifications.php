860af897f7fcba48048ac5e92a336af1fa116ab2
NicoMachado <nico.machado@gmail.com>
2020-10-02 16:13:35 -0300

    Integrando

--- notifications.php
+++ notifications.php
@@ -2,20 +2,23 @@
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 
-    $file = fopen('log.txt','w');
+    $file = fopen('log.txt','a');
     fwrite($file, "Entre...." );
-    fwrite($file, var_dump($_REQUEST['type']));
+    fclose($file);
+
+    $file = fopen('log.txt','a');
+    fwrite($file, "Entre...." . print_r($_REQUEST['type'],true));
     fclose($file);
 
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-    if (isset($_POST["type"]) ) {
-        switch($_POST["type"]) {
+    if (isset($_REQUEST["type"]) ) {
+        switch($_REQUEST["type"]) {
             case "payment":
     
                 $json = file_get_contents('php://input');
                 // Converts it into a PHP object
                 $data = json_decode($json);
-                $file = fopen('mp_payment_type.json','w');
+                $file = fopen('mp_payment_type.json','a');
                 fwrite($file, $json);
                 fclose($file);
             
