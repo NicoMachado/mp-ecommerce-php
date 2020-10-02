061037693896b922087cf0936b190c8dad7cf774
NicoMachado <nico.machado@gmail.com>
2020-10-02 15:53:29 -0300

    Integrando

--- notifications.php
+++ notifications.php
@@ -3,44 +3,38 @@
 require __DIR__ .  '/vendor/autoload.php';
 
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-    $file = fopen('log.txt','w');
+    $file = fopen('log.txt','w+');
     fwrite($file, "Entre...." );
     fwrite($file, var_dump($_POST));
     fclose($file);
 
-
-    $json = file_get_contents('php://input');
-    // Converts it into a PHP object
-    $data = json_decode($json);
-    $file = fopen('mp_payment_type.json','w');
-    fwrite($file, $json);
-    fclose($file);
-
-
-    switch($_POST["type"]) {
-        case "payment":
-
-            $json = file_get_contents('php://input');
-            // Converts it into a PHP object
-            $data = json_decode($json);
-            $file = fopen('/mp_payment_type.json','w');
-            fwrite($file, $json);
-            fclose($file);
-
-
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            //Save Response to loca disc
-//            var_dump($payment);
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-            break;
+    if (isset($_POST["type"]) ) {
+        switch($_POST["type"]) {
+            case "payment":
+    
+                $json = file_get_contents('php://input');
+                // Converts it into a PHP object
+                $data = json_decode($json);
+                $file = fopen('mp_payment_type.json','w');
+                fwrite($file, $json);
+                fclose($file);
+            
+    
+                $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+                //Save Response to loca disc
+    //            var_dump($payment);
+                break;
+            case "plan":
+                $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+                break;
+            case "subscription":
+                $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+                break;
+            case "invoice":
+                $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+                break;
+        }
+    
     }
 
 ?>
