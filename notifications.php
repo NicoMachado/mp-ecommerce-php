cc32ee54e481df9dbb69284f0631a176bb391cfd
NicoMachado <nico.machado@gmail.com>
2020-10-02 13:26:14 -0300

    Integrando

--- notifications.php
+++ notifications.php
@@ -3,6 +3,9 @@
 require __DIR__ .  '/vendor/autoload.php';
 
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+    $file = fopen('log.txt','w');
+    fwrite($file, "Entre....");
+    fclose($file);
 
     switch($_POST["type"]) {
         case "payment":
