ee0d8bf116895369d61f9a1cb59c4ff5c701edfd
NicoMachado <nico.machado@gmail.com>
2020-10-02 10:56:40 -0300

    Integrando

--- detail.php
+++ detail.php
@@ -207,6 +207,7 @@ $preference->save();
 
                                    <!--  <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button> -->
                                    <a href="<?php echo $preference->init_point; ?>">Pagar la compra</a>
+                                   <?php echo $preference->id; ?>
 <!--
                                     <form action="/procesar-pago" method="POST">
  
