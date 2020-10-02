5c608cc30e75e489fc9d464447d65925d6953756
NicoMachado <nico.machado@gmail.com>
2020-10-02 17:03:17 -0300

    Integrando

--- success.php
+++ success.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
+
+<html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   
+    <meta name="viewport" content="width=1024">
+    <title>Tienda e-commerce</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="format-detection" content="telephone=no">
+
+    <script src="https://www.mercadopago.com/v2/security.js" view="item"></script>
+
 <!--
 collection_id=[PAYMENT_ID]&collection_status=approved&external_ref
 erence=[EXTERNAL_REFERENCE]&payment_type=credit_card&preference_id=[PREFERENCE_ID]&site_id
@@ -21,3 +32,5 @@ $url = $_SERVER['QUERY_STRING'];
 parse_str( parse_url( $url, PHP_URL_QUERY), $array );
 print_r( $array );
 ?>
+
+</html>
