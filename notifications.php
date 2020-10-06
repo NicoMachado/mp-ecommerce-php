9bf4a472feda69d500f609013750d0d12d8a6113
NicoMachado <nico.machado@gmail.com>
2020-10-06 20:24:00 -0300

    Integrando PHP

--- notifications.php
+++ notifications.php
@@ -10,7 +10,7 @@ $file = fopen('log.txt','w');
 
 fwrite($file, "Entre....>" . $param['type'] .'<...');
 
-fwrite($file, "Entre....>" . $param .'<...');
+fwrite($file, "Entre....>" . print_r($param) .'<...');
 fclose($file);
 
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
@@ -19,7 +19,7 @@ fclose($file);
     
             
     
-                $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+                $payment = MercadoPago\Payment.find_by_id($param["id"]);
                 //Save Response to loca disc
     //            var_dump($payment);
     $file = fopen('mp_payment_type.json','w');
