991a631366aa10f2f0ca5698ce01864f64e59c42
NicoMachado <nico.machado@gmail.com>
2020-10-06 20:13:54 -0300

    Integrando PHP

--- notifications.php
+++ notifications.php
@@ -10,6 +10,7 @@ $file = fopen('log.txt','w');
 
 fwrite($file, "Entre....>" . $param['type'] .'<...');
 
+fwrite($file, "Entre....>" . $param .'<...');
 fclose($file);
 
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
